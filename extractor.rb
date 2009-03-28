c04fbba2b16a0e59fb1090ba0e5b97ef2ec6e67b
Daniel Haran <chebuctonian@mgmail.com>
2009-03-28 16:32:33 -0400

    passing intervention extraction

--- extractor.rb
+++ extractor.rb
@@ -15,16 +15,35 @@ class Extractor
     p = find_following(element, :p)
     
     intervention.link = (p / "//a[@class='WebOption'").first.attributes["href"]
+    intervention.paragraphs = [extract_first_paragraph(p), *extract_following_paragraphs(p) ]
     
     intervention
+  end
+  
+  def extract_first_paragraph(p)
+    (p / "div/div").remove
+    strip_inner(p)
+  end
+  
+  def extract_following_paragraphs(p)
+    paras = []
+    while (p = p.next_sibling.next_sibling) do
+      break unless p.name == 'p'
+      paras << strip_inner(p)
+    end
+    paras
   end
   
   def find_following(element, element_type)
     10.times do
       element = element.next_sibling
-      puts element.inspect
       return element if element.name == element_type.to_s
     end
     raise "oops"
   end
+  
+  private
+    def strip_inner(el)
+      el.inner_text.strip.gsub(/^\?*/,'')
+    end
 end
